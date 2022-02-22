4acd4ace12c1bcccb4824237d6ba8909209d81c7
zhangyingzheng <zhangyingzheng@meituan.com>
2022-02-22 19:21:00 +0800

    Initial commit

---
+++ src/main/java/com/example/democsc/DemocscApplication.java
@@ -0,0 +1,13 @@
+package com.example.democsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemocscApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemocscApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/democsc/DemocscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.democsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemocscApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
